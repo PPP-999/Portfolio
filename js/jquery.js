88bcd6325cc2f8af54acb94ac75c376027d6053b
PPP-999 <y.h.fa.0311@gmail.com>
2019-12-10 10:28:53 +0900

    モーダル時の背景スクロールの無効化

--- js/jquery.js
+++ js/jquery.js
@@ -38,3 +38,62 @@ $(function(){
         return false;
     });
 });
+
+
+
+$(function(){
+    var scrollPosition;
+    $(".modalOpener").on("click", function() {
+        scrollPosition = $(window).scrollTop();
+        $('body').addClass('fixed').css({'top': -scrollPosition});
+    });
+    $(".modalCloser").on("click", function() {
+        $('body').removeClass('fixed').css({'top': 0});
+        window.scrollTo( 0 , scrollPosition );
+    });
+});
+
+
+
+$(function(){
+
+    var effect_btm = 300;
+    var effect_move = 50;
+    var effect_time = 800;
+
+    $('.scroll-fade-row').css({
+        opacity: 0
+    });
+    $('.scroll-fade-row').children().each(function(){
+        $(this).css({
+            opacity: 0,
+            transform: 'translateY('+ effect_move +'px)',
+            transition: effect_time + 'ms'
+        });
+    });
+
+    $(window).on('scroll load', function(){
+        var scroll_top = $(this).scrollTop();
+        var scroll_btm = scroll_top + $(this).height();
+        var effect_pos = scroll_btm - effect_btm;
+
+        $('.scroll-fade-row').each( function() {
+            var this_pos = $(this).offset().top;
+            if ( effect_pos > this_pos ) {
+                $(this).css({
+                    opacity: 1,
+                    transform: 'translateY(0)'
+                });
+                $(this).children().each(function(i){
+                    $(this).delay(100 + i*200).queue(function(){
+                        $(this).css({
+                            opacity: 1,
+                            transform: 'translateY(0)'
+                        }).dequeue();
+                    });
+                });
+            }
+        });
+    });
+
+});
